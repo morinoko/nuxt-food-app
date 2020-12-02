4b6ed24720e95a8ea7f27a10a6a87b23e8133b4c
Felice Forby <forby.1@gmail.com>
2020-12-03 08:16:22 +0900

    Add restaurants page

---
+++ pages/restaurants.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="restaurant">
+    <h1 class="restaurantheading">Restaurants</h1>
+    <AppRestaurantInfo />
+  </section>
+</template>
+
+<script>
+import AppRestaurantInfo from "@/components/AppRestaurantInfo";
+
+export default {
+  components: {
+    AppRestaurantInfo,
+  }
+}
+</script>
+
+<style scoped>
+</style>
