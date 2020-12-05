532b50c6e83e7e6e5996655f2bc459fe98c20a59
Felice Forby <forby.1@gmail.com>
2020-12-05 11:06:47 +0900

    Add stubbed cart page and update logo to use SVG

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -1,14 +1,21 @@
 <template>
   <nav>
     <ul>
-      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/"><AppLogo /></nuxt-link></li>
       <li><nuxt-link to="/restaurants">Restaurants</nuxt-link></li>
     </ul>
+    <nuxt-link to="/cart">Cart</nuxt-link>
   </nav>
 </template>
 
 <script>
-export default {}
+import AppLogo from "@/components/AppLogo";
+
+export default {
+  components: {
+    AppLogo
+  }
+}
 </script>
 
 <style lang="scss" scoped>
